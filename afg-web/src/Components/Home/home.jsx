92da08233f02b36051df9257e0e6b711efb086f3
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2023-12-06 02:26:04 +0430

    ant added

--- afg-web/src/Components/Home/home.jsx
+++ afg-web/src/Components/Home/home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "../Home/home.css";
+import { Carousel } from "antd";
 
 const Home = () => {
+  // const contentStyle = {
+  //   height: "160px",
+  //   color: "#fff",
+  //   lineHeight: "160px",
+  //   textAlign: "center",
+  //   background: "#364d79",
+  // };
+
   return (
     <section className="home">
       <div className="secContatiner container">
@@ -15,6 +24,23 @@ const Home = () => {
             <a href="#">Explore Now!</a>
           </button>
         </div>
+      </div>
+
+      <div className="homeCard grid">
+        <Carousel>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+        </Carousel>
       </div>
     </section>
   );

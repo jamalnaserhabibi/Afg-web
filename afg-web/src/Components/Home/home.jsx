d801e8d3e9815df0d14396d57316928ff3f964ae
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2023-12-06 03:59:55 +0430

    done

--- afg-web/src/Components/Home/home.jsx
+++ afg-web/src/Components/Home/home.jsx
@@ -3,13 +3,14 @@ import "../Home/home.css";
 import { Carousel } from "antd";
 
 const Home = () => {
-  // const contentStyle = {
-  //   height: "160px",
-  //   color: "#fff",
-  //   lineHeight: "160px",
-  //   textAlign: "center",
-  //   background: "#364d79",
-  // };
+  const contentStyle = {
+    height: "40vh",
+    color: "black",
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  };
 
   return (
     <section className="home">
@@ -17,7 +18,7 @@ const Home = () => {
         <div className="homeText">
           <h1 className="title">Afghanistan | The Unexplored</h1>
           <p className="subTitle">
-            Unveiling the Rich Diversity of Culture, Landscapes, and Stories Yet
+            Unveiling the Rich Diversity of History, Culture, Landscapes, and Stories Yet
             to be Discovered
           </p>
           <button className="btn">
@@ -26,21 +27,21 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="homeCard grid">
-        <Carousel>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-        </Carousel>
+      <div className="homeCard">
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>Afghanistannnn</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Afghanistannnn</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>Afghanistannnn</h3>
+        </div>
+  </Carousel>
       </div>
     </section>
   );

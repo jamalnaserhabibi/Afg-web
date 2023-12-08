85174195da01308990f9aa16360245b749d9662d
jamalnaserhabibi <jamalnaser.tech@gmail.com>
2023-12-08 04:32:14 +0430

    aos added

--- afg-web/src/Components/City/city.jsx
+++ afg-web/src/Components/City/city.jsx
@@ -1,82 +1,98 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../City/city.css";
 import "../../App.css";
+import { FaUserAlt } from "react-icons/fa";
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import {} from "react-icons/bs";
 // import { Carousel } from "antd";
 
-import img2 from "../../Assets/city.jpg";
+import kabul from "../../Assets/masij.jpg";
+import balkh from "../../Assets/balkh.jpg";
+import herat from "../../Assets/bamyan.jpg";
+import kandahar from "../../Assets/river.jpg";
+
 const Data = [
   {
     id: 1,
-    img: img2,
-    location: "Kabul Jan",
-    desTitle: "Nothing Now",
-    grade: 1,
+    img: kabul,
+    location: "Kabul ",
+    desTitle: "Kabul City",
+    des: "The largest city of Afghanistan, major center for politics, culture, and commerce. Serves as the economic and political heart of the country.",
+    grade: "7.2M",
   },
+
   {
-    id: 1,
-    img: img2,
-    location: "Kabul Jan",
-    desTitle: "Nothing Now",
-    grade: 1,
+    id: 2,
+    img: herat,
+    location: "Herat",
+    desTitle: "Herat City",
+    des: "Known for its rich history and architectural heritage, Herat is the third-largest city in Afghanistan. It has a vibrant cultural scene",
+    grade: "3.1M",
   },
+
   {
-    id: 1,
-    img: img2,
-    location: "Kabul Jan",
-    desTitle: "Nothing Now",
-    grade: 1,
+    id: 3,
+    img: balkh,
+    location: "Balkh",
+    desTitle: "Mazar-i-Sharif City",
+    des: "Mazar-i-Sharif is renowned for the Blue Mosque (Mazar-i-Sharif Shrine), making it a significant religious and cultural destination",
+    grade: "2.5M",
   },
+
   {
-    id: 1,
-    img: img2,
-    location: "Kabul Jan",
-    desTitle: "Nothing Now",
-    grade: 1,
+    id: 4,
+    img: kandahar,
+    location: "Kandahar",
+    desTitle: "Qandahar City",
+    des: "The second-largest city, holds historical and cultural importance. It has played a significant role in Afghan history",
+    grade: "2.3M",
   },
 ];
+
 const city = () => {
+  Aos.init({ duration: 1000 }, []);
   return (
     <section className="city section container">
       <div className="secContainer">
         <div className="secHeader flex">
-          <div className="textDiv">
-            <h1 className="secTitle">Popular Places</h1>
+          <div data-aos="fade-up" className="textDiv">
+            <h1 className="secTitle">Popular Cities</h1>
             <p className="secText">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et vitae
-              eligendi magnam. Voluptatibus consequatur dolorem saepe eveniet
-              harum quo assumenda minus nobis sunt officia? Voluptas expedita
-              natus tempora corporis obcaecati!
+              In Afghanistan, cities aren't just places; they're stories etched
+              with emotion. Some hold the pulse of modern life, others cradle
+              ancient tales. Each city whispers its unique history, weaving
+              together a tapestry that resonates with the heart of Afghanistan's
+              past and present.
             </p>
           </div>
-          <div className="iconsDiv flex">
-            <BsArrowLeftShort className="icon" />
-            <BsArrowRightShort className="icon" />
+          <div data-aos="fade-up" className="iconsDiv flex">
+            {/* <BsArrowLeftShort className="icon" />
+            <BsArrowRightShort className="icon" /> */}
           </div>
         </div>
         <div className="mainContent grid">
-          {Data.map(() => {
+          {Data.map(({ id, img, location, grade, desTitle, des }) => {
             return (
-              <div className="singleDestination">
+              <div data-aos="fade-up" className="singleDestination">
                 <div className="destImage">
-                  <img src={img2} alt="The Kabul" />
+                  <img src={img} alt="The Kabul" />
                   <div className="overlaysInfo">
-                    <h3>The Capital</h3>
-                    <p>
-                      With Over Ten Millions Population Is The Capital Of
-                      Afghanistan
-                    </p>
+                    <h3>{desTitle}</h3>
+                    <p>{des}</p>
                     <BsArrowRightShort className="icon" />
                   </div>
                 </div>
                 <div className="destFooter">
-                  <div className="number">01</div>
+                  <div className="number">0{id}</div>
                   <div className="destText flex">
-                    <h6 className="text">KABUL</h6>
+                    <h6 className="text">{location}</h6>
                     <span className="flex">
-                      <span className="icon">●</span>
-                      <span>The Capital</span>
+                      <span className="icon">
+                        <FaUserAlt />
+                      </span>
+                      <span>{grade}</span>
                     </span>
                   </div>
                 </div>
--- afg-web/src/Components/Home/home.jsx
+++ afg-web/src/Components/Home/home.jsx
@@ -3,12 +3,19 @@ import "../Home/home.css";
 import { Carousel } from "antd";
 import img2 from "../../Assets/bgimage.jpg";
 import img3 from "../../Assets/city2.jpg";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Home = () => {
+  Aos.init({ duration: 2000, delay: 1000 }, []);
+
   return (
     <section className="home">
-      <div className="secContatiner container">
+      <div data-aos="fade-up" className="secContatiner container">
         <div id="title" className="homeText">
-          <h1 className="title">Afghanistan | The Unexplored</h1>
+          <h1 data-aos="fade-up" className="title">
+            Afghanistan | The Unexplored
+          </h1>
           <p className="subTitle">
             Unveiling the Rich Diversity of History, Culture, Landscapes, and
             Stories Yet to be Discovered
